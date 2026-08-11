89ba8b0e32c7e9f521b42d1ec5613084c08de660
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-08-11 01:26:42 +0100

    feat(auth): register custom user model in django admin with role filters and fieldsets

--- server/apps/authentication/admin.py
+++ server/apps/authentication/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
 
 # Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role', "is_staff", "is_active")
+    list_filter = ("role", "is_staff", "is_superuser", "is_active")
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Info', {'fields': ('role', 'phone_number')}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Extra Info', {'fields': ('role', 'phone_number')}),
+    )
