1dedf5d5ff6cb80d95433620fcdfe6578742db77
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-08-12 23:49:16 +0100

    fix: django secret key

--- server/config/settings.py
+++ server/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key-at-least-32-chars-long')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
